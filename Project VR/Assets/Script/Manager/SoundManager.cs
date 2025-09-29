63349760e57e52116279b737184a164482ae72fb
Victor <victor.baz92@gmail.com>
2025-09-29 21:24:31 +0200

    SoundManager Setup

---
+++ Project VR/Assets/Script/Manager/SoundManager.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+namespace Script.Manager
+{
+    public class SoundManager : MonoBehaviour
+    {
+         private static SoundManager instance;
+        
+        public AudioSource audioSource;
+
+        [Range(0,1)] public float masterVolume = 1f;
+
+        
+        public static SoundManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindFirstObjectByType<SoundManager>();
+                    
+                    if (instance == null)
+                    {
+                        GameObject soundManager = new GameObject("SoundManager");
+                        instance = soundManager.AddComponent<SoundManager>();
+                    }
+                }
+                return instance;
+            }
+        }
+        
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        
+        private void Start()
+        {
+            audioSource.volume = 1f; }
+        
+        
+        public void PlayMusicOneShot(AudioClip _audioClip)
+        {
+            if (_audioClip == null)
+            {
+                Debug.LogError("The audioClip you tried to play is null");
+                return;
+            }
+            audioSource.PlayOneShot(_audioClip);
+        }
+        
+        
+        public void UpdateMasterVolume(float volume)
+        {
+            masterVolume = volume;
+            audioSource.volume = masterVolume;
+        }
+
+        public GameObject InitialisationAudioObjectDestroyAtEnd(AudioClip audioClipTarget, bool looping, 
+            bool playingAwake, float volumeSound, string _name)
+        {
+            GameObject emptyObject = new GameObject(_name);
+            emptyObject.transform.SetParent(gameObject.transform);
+
+            AudioSource audioSourceGeneral = emptyObject.AddComponent<AudioSource>();
+            audioSourceGeneral.clip = audioClipTarget;
+            audioSourceGeneral.loop = looping;
+            audioSourceGeneral.playOnAwake = playingAwake;
+            audioSourceGeneral.volume = volumeSound * masterVolume;
+            audioSourceGeneral.Play();
+            
+            if (!looping)
+            {
+                Destroy(emptyObject, audioClipTarget.length);
+            }
+            
+            return emptyObject;
+        }
+    }
+}
