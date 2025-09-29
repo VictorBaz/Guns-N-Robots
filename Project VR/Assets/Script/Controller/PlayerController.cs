4466362d0b57171831a6b07b29c29b9ad9fabcc8
BAZ Victor <v.baz@rubika-edu.com>
2025-09-29 17:13:09 +0200

    Conitnue Ticking Player

---
+++ Project VR/Assets/Script/Controller/PlayerController.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Script.Enum;
+using Script.Manager;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Script.Controller
+{
+    public class PlayerController : MonoBehaviour
+    {
+        #region Fields
+
+        private List<BarrelHoleState> barel = new List<BarrelHoleState>();
+
+        private BarrelHoleState currentBarrelHole;
+
+        private int indexInBarel = 0;
+
+        private BarelManager barelManager = new BarelManager();
+
+        #endregion
+
+
+        private void Start()
+        {
+            barelManager.SetupBarrel(barel);
+        }
+
+        #region Observer
+
+        private void OnEnable()
+        {
+            TickManager.OnTick += GetCurrentBarrelHoleByTick;
+        }
+
+        private void OnDisable()
+        {
+            TickManager.OnTick -= GetCurrentBarrelHoleByTick;
+        }
+
+        #endregion
+
+        private void GetCurrentBarrelHoleByTick()
+        {
+            indexInBarel = barelManager.IncrementBarrelByTick(barel, indexInBarel);
+            currentBarrelHole = barel[indexInBarel];
+            Debug.Log(indexInBarel);
+        }
+    }
+}
--- Project VR/Assets/Script/Manager/BarelManager.cs
+++ Project VR/Assets/Script/Manager/BarelManager.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 namespace Script.Manager
 {
-    public class BarelManager : MonoBehaviour
+    public class BarelManager
     {
-        private void SetupBarrel(List<BarrelHoleState> barel)
+        public void SetupBarrel(List<BarrelHoleState> barel)
         {
             barel.Clear();
-            const int numberOfHoles = 5;
+            const int numberOfHoles = 6;
 
             for (int i = 0; i < numberOfHoles; i++)
             {
                 barel.Add((BarrelHoleState)Random.Range((int)BarrelHoleState.Empty,(int)BarrelHoleState.Full + 1));
             }
+            
+            //TODO dont forget to check if all full or all empty
+            
             //Done Setup
+        }
+        
+        public int IncrementBarrelByTick(List<BarrelHoleState> barel, int actualIndex)
+        {
+            int newIndex = actualIndex + 1;
+            
+            if (newIndex > barel.Count - 1) // then should iterate
+            {
+                return 0;
+            }
+            else
+            {
+                return newIndex;
+            }
         }
     }
 }
