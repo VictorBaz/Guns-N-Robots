deabf4b12bb848f8fbe0e69ab8b9ab36f6e067ea
Victor <victor.baz92@gmail.com>
2025-10-17 00:40:53 +0200

    refactor PlayerController

--- Project VR/Assets/Script/Controller/PlayerController.cs
+++ Project VR/Assets/Script/Controller/PlayerController.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq.Expressions;
 using Script.Enum;
 using Script.Interface;
 using Script.Manager;
 using Unity.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
@@ -15,38 +13,50 @@ namespace Script.Controller
 {
     public class PlayerController : MonoBehaviour
     {
-        #region Fields
+        #region Serialized Fields
 
         [SerializeField] private VisualsController visuals;
+        [SerializeField] private LineRenderer lineRenderer;
+        [SerializeField] private Grabing grabing;
+        [SerializeField] private Transform bulletOrigin;
+        [SerializeField] private XRIDefaultInputActions inputActions;
+        [SerializeField] private XRInputValueReader<float> m_TriggerInput = new XRInputValueReader<float>("Trigger");
+        [SerializeField] private float minSpeedToReload;
+        [SerializeField, ReadOnly] private List<CylinderHoleState> cylinder = new List<CylinderHoleState>();
 
-        [field: SerializeField, ReadOnly] private List<CylinderHoleState> cylinder = new List<CylinderHoleState>();
+        #endregion
+
+        #region Public Properties
 
         public CylinderHoleState currentCylinderHole { get; private set; }
 
-        private int indexInBarel = 0;
+        #endregion
+
+        #region Private Fields
 
         private CylinderManager cylinderManager = new CylinderManager();
+        private LayerMask layerMask;
+        private RaycastHit hit;
+        
+        private int indexInBarel = 0;
+        private bool hasShot = false;
+        private bool canShoot = false;
+        private bool reloading = false;
+        
+        private Vector3 startReloadPosition;
+        private Vector3 endReloadPosition;
+        private float startTimeReload;
+        private float endTimeReload;
+        private Coroutine currentCoroutineReloading;
+
+        #endregion
+
+        #region Events
 
         public static Action OnplayerShoot;
         public static Action OnPlayerMissedShot;
         public static Action OnPlayerReload;
-        
-        [SerializeField]
-        XRInputValueReader<float> m_TriggerInput = new XRInputValueReader<float>("Trigger");
 
-        [SerializeField] private XRIDefaultInputActions inputActions;
-
-        private bool hasShot = false;
-        [SerializeField] private LineRenderer lineRenderer;
-        private RaycastHit hit;
-
-        [SerializeField] private Grabing grabing;
-        LayerMask layerMask;
-        private bool canShoot;
-        [SerializeField] private Transform bulletOrigin;
-
-        private bool reloading = false;
-        
         #endregion
 
         #region Unity Methods
@@ -54,7 +64,7 @@ namespace Script.Controller
         private void Awake()
         {
             cylinderManager.SetupBarrel(cylinder);
-            layerMask = LayerMask.GetMask( "Head", "Wall");
+            layerMask = LayerMask.GetMask("Head", "Wall");
             inputActions = new XRIDefaultInputActions();
         }
 
@@ -65,22 +75,9 @@ namespace Script.Controller
 
         private void Update()
         {
+            HandleTriggerInput();
             PlayerFire();
-            if (m_TriggerInput != null)
-            {
-                var triggerVal = m_TriggerInput.ReadValue();
-            
-                if (triggerVal < 0.0001f && grabing.isGunInHand)
-                {
-                    canShoot = true;
-                }
-            }
         }
-        
-
-        #endregion
-
-        #region Observer
 
         private void OnEnable()
         {
@@ -93,130 +90,166 @@ namespace Script.Controller
 
         private void OnDisable()
         {
-            inputActions.Dispose();
             inputActions.Disable();
+            inputActions.Dispose();
             TickManager.OnTick -= GetCurrentBarrelHoleByTick;
             MiniGameManager.OnRoundEnd -= ResetPlayerAfterRound;
             inputActions.XRIRightInteraction.ReloadButton.started -= ReloadHandleStart;
             inputActions.XRIRightInteraction.ReloadButton.canceled -= ReloadHandleStart;
-     
         }
 
         #endregion
 
-        #region Player Methods
+        #region Input Handling
 
-        private void PlayerFire() 
+        private void HandleTriggerInput()
         {
-            if (reloading) return;
-            
-            //TODO LINK THAT WITH GRABBING 
+            if (m_TriggerInput == null) return;
+
             var triggerVal = m_TriggerInput.ReadValue();
-            if (triggerVal > 0 && 
-                GameManager.Instance.CurrentState == GameState.MiniGameRunning &&
-                !hasShot && canShoot)
+            
+            if (triggerVal < 0.0001f && grabing.isGunInHand)
             {
-                hasShot = true;
-                switch (currentCylinderHole)
-                {
-                    case CylinderHoleState.Empty:
-                        //future needed
-                        break;
-                    case CylinderHoleState.Full:
-                        canShoot = false;
-                        visuals.Shoot();
-                        visuals.Muzzle();
-                        visuals.BulletShell(); 
-                        if (Physics.Raycast(bulletOrigin.position, bulletOrigin.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-                        {
-                            lineRenderer.enabled = true;
-                            lineRenderer.SetPosition(0, bulletOrigin.position);
-                            lineRenderer.SetPosition(1, hit.point);
-                            
-                            visuals.Sparks(hit.point,hit.normal);
-                            
-                            hit.transform.GetComponent<IDamagable>()?.TakeDamage();
-                        }
-                        cylinder[indexInBarel] = CylinderHoleState.Empty;
-                        OnplayerShoot?.Invoke();
-                        break;
-                }
+                canShoot = true;
             }
         }
-        
+
+        #endregion
+
+        #region Shooting
+
+        private void PlayerFire()
+        {
+            if (reloading) return;
+            if (!CanFireWeapon()) return;
+
+            hasShot = true;
+
+            if (currentCylinderHole == CylinderHoleState.Full)
+            {
+                ExecuteShot();
+            }
+        }
+
+        private bool CanFireWeapon()
+        {
+            var triggerVal = m_TriggerInput.ReadValue();
+            return triggerVal > 0 &&
+                   GameManager.Instance.CurrentState == GameState.MiniGameRunning &&
+                   !hasShot &&
+                   canShoot;
+        }
+
+        private void ExecuteShot()
+        {
+            canShoot = false;
+            
+            visuals.Shoot();
+            visuals.Muzzle();
+            visuals.BulletShell();
+
+            if (Physics.Raycast(bulletOrigin.position, bulletOrigin.TransformDirection(Vector3.forward), 
+                out hit, Mathf.Infinity, layerMask))
+            {
+                DisplayBulletTracer();
+                visuals.Sparks(hit.point, hit.normal);
+                hit.transform.GetComponent<IDamagable>()?.TakeDamage();
+            }
+
+            cylinder[indexInBarel] = CylinderHoleState.Empty;
+            OnplayerShoot?.Invoke();
+        }
+
+        private void DisplayBulletTracer()
+        {
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, bulletOrigin.position);
+            lineRenderer.SetPosition(1, hit.point);
+        }
+
+        #endregion
+
+        #region Barrel Management
+
         private void GetCurrentBarrelHoleByTick()
         {
-            /*if (!hasShot && currentCylinderHole == CylinderHoleState.Full)
-            {
-                OnPlayerMissedShot?.Invoke(); 
-            }*/
-            
             indexInBarel = cylinderManager.IncrementBarrelByTick(cylinder, indexInBarel);
             currentCylinderHole = cylinder[indexInBarel];
             hasShot = false;
 
-            SoundManager.Instance?.PlayMusicOneShot(currentCylinderHole == CylinderHoleState.Empty
-                ? SoundManager.Instance.emptyLoad
-                : SoundManager.Instance.fullLoad);
+            PlayBarrelSound();
         }
 
-        #endregion
+        private void PlayBarrelSound()
+        {
+            if (SoundManager.Instance == null) return;
 
-        #region Getter Setter
+            var soundToPlay = currentCylinderHole == CylinderHoleState.Empty
+                ? SoundManager.Instance.emptyLoad
+                : SoundManager.Instance.fullLoad;
+
+            SoundManager.Instance.PlayMusicOneShot(soundToPlay);
+        }
 
         public List<CylinderHoleState> GetBarrel()
         {
             return cylinder;
         }
 
-        private void ResetPlayerAfterRound()
-        {
-            indexInBarel = 0;
-            hasShot = false;
-        }
-        
         #endregion
 
-        #region Reload Methods
-
-        private Vector3 startReloadPosition;
-        private float startTimeReload;
-        private Vector3 endReloadPosition;
-        private float endTimeReload;
-
-        [SerializeField] private float minSpeedToReload;
+        #region Reload System
 
         private void ReloadHandleStart(InputAction.CallbackContext ctx)
         {
             if (reloading) return;
-            
+
             if (ctx.started)
             {
-                startReloadPosition = bulletOrigin.transform.position;
-                startTimeReload = Time.time;
+                RecordReloadStartPosition();
             }
-            if (ctx.canceled)
+            else if (ctx.canceled)
             {
-                endReloadPosition = bulletOrigin.transform.position;
-                endTimeReload = Time.time;
-                float distance = Vector3.Distance(startReloadPosition, endReloadPosition);
-                float time = endTimeReload - startTimeReload;
-
-                float speedOfGun = distance / time; // V= D/T
-                Debug.Log($"speed of gun : {speedOfGun}, distance : {distance}, time : {time}");
-
-                if (speedOfGun > minSpeedToReload) //then reload
-                {
-                    float timeReload = visuals.Reload();
-                    currentCoroutineReloading = StartCoroutine(ToggleReloadState(timeReload));
-                    cylinderManager.Reload(cylinder);
-                    OnPlayerReload?.Invoke();
-                }
+                TryExecuteReload();
             }
         }
 
-        private Coroutine currentCoroutineReloading;
-        IEnumerator ToggleReloadState(float time)
+        private void RecordReloadStartPosition()
+        {
+            startReloadPosition = bulletOrigin.transform.position;
+            startTimeReload = Time.time;
+        }
+
+        private void TryExecuteReload()
+        {
+            endReloadPosition = bulletOrigin.transform.position;
+            endTimeReload = Time.time;
+
+            float reloadSpeed = CalculateReloadSpeed();
+            Debug.Log($"speed of gun : {reloadSpeed}, distance : {Vector3.Distance(startReloadPosition, endReloadPosition)}, time : {endTimeReload - startTimeReload}");
+
+            if (reloadSpeed > minSpeedToReload)
+            {
+                ExecuteReload();
+            }
+        }
+
+        private float CalculateReloadSpeed()
+        {
+            float distance = Vector3.Distance(startReloadPosition, endReloadPosition);
+            float time = endTimeReload - startTimeReload;
+            return distance / time; // V = D/T
+        }
+
+        private void ExecuteReload()
+        {
+            float timeReload = visuals.Reload();
+            currentCoroutineReloading = StartCoroutine(ToggleReloadState(timeReload));
+            cylinderManager.Reload(cylinder);
+            OnPlayerReload?.Invoke();
+        }
+
+        private IEnumerator ToggleReloadState(float time)
         {
             reloading = true;
             yield return new WaitForSeconds(time);
@@ -224,7 +257,15 @@ namespace Script.Controller
         }
 
         #endregion
-        
-        
+
+        #region Round Management
+
+        private void ResetPlayerAfterRound()
+        {
+            indexInBarel = 0;
+            hasShot = false;
+        }
+
+        #endregion
     }
 }
