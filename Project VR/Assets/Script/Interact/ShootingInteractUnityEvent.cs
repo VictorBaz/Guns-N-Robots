e5d09353053c006168d934268b187ca418a6af34
Victor <victor.baz92@gmail.com>
2026-01-04 15:00:32 +0100

    unity event zebii

---
+++ Project VR/Assets/Script/Interact/ShootingInteractUnityEvent.cs
@@ -0,0 +1,15 @@
+using Script.Interface;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ShootingInteractUnityEvent : MonoBehaviour,IDamagable
+{
+    [SerializeField] private UnityEvent eventToDo;
+    
+    public void TakeDamage()
+    {
+        eventToDo?.Invoke();
+    }
+
+    public bool CanTakeDamage { get; set; } = true;
+}
